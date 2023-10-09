3ea25f9878fb4777baf19fa327ba399fd9cea3bd
HyunWoo <kimjd766@gmail.com>
2023-10-09 23:45:50 +0900

    start project

---
+++ baemin/src/test/java/hyunwoo/baemin/BaeminApplicationTests.java
@@ -0,0 +1,13 @@
+package hyunwoo.baemin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
